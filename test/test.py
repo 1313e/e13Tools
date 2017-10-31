92360dc17ed44980963888ba5636768acb4c9cb0
Ellert van der Velden <ellert_vandervelden@outlook.com>
2017-10-31 17:57:02 +1100

    Update test.py

--- test/test.py
+++ test/test.py
@@ -4,5 +4,11 @@ Testing script for travis
 
 """
 
-import pytest
-import e13tools as e13
+def tests():
+  # Check if all modules can be imported
+  import e13tools as e13
+  import e13tools.pyplot as e13plt
+  import e13tools.sampling as e13spl
+  
+if(__name__ == '__main__'):
+  tests()
