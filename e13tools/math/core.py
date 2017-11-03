167518ff01fa91db78e489cc0956505bcd5018d9
1313e <ellert_vandervelden@outlook.com>
2017-11-03 13:58:54 +1100

    Updated to patch 0.2.0a2. Implemented support for complex matrices in the nearest_PD function.

--- e13tools/math/core.py
+++ e13tools/math/core.py
@@ -15,7 +15,7 @@ from __future__ import division, absolute_import, print_function
 from e13tools import ShapeError
 import numpy as np
 
-__all__ = ['is_PD', 'nearest_SPD', 'transposeC']
+__all__ = ['is_PD', 'nearest_PD', 'transposeC']
 
 
 # %% FUNCTIONS
@@ -37,7 +37,7 @@ def is_PD(matrix):
 
     Examples
     --------
-    Using a matrix that is positive-definite (like the identity matrix):
+    Using a real matrix that is positive-definite (like the identity matrix):
 
     >>> matrix = np.eye(3)
     >>> matrix
@@ -48,19 +48,40 @@ def is_PD(matrix):
     True
 
 
-    Using a matrix that is not positive-definite (or symmetric):
+    Using a real matrix that is not symmetric (Hermitian):
 
     >>> matrix = np.array([[1, 2], [3, 4]])
     >>> matrix
     array([[1, 2],
            [3, 4]])
     >>> is_PD(matrix)
-    False
+    ValueError: Input matrix must be Hermitian!
+
+
+    Using a complex matrix that is positive-definite:
+
+    >>> matrix = np.array([[4, 1.5+1j], [1.5-1j, 3]])
+    >>> matrix
+    array([[ 4.0+0.j,  1.5+1.j],
+           [ 1.5-1.j,  3.0+0.j]])
+    >>> is_PD(matrix)
+    True
 
     """
 
+    # Check if input is a matrix
+    if(np.shape(np.shape(matrix))[0] != 2):
+        raise ShapeError("Input must be two-dimensional!")
+    else:
+        rows, columns = np.shape(matrix)
+
+    # Check if matrix is a square
+    if(rows != columns):
+        raise ShapeError("Input matrix has shape [%s, %s]. Input matrix must "
+                         "be a square matrix!" % (rows, columns))
+
     # Check if matrix is Hermitian
-    if((transposeC(matrix) != matrix).all()):
+    if not((transposeC(matrix) == matrix).all()):
         raise ValueError("Input matrix must be Hermitian!")
 
     # Try to use Cholesky on matrix. If it fails,
@@ -72,24 +93,23 @@ def is_PD(matrix):
         return(True)
 
 
-def nearest_SPD(matrix):
+def nearest_PD(matrix):
     """
-    Find the nearest symmetric positive-definite matrix to the input `matrix`.
+    Find the nearest positive-definite matrix to the input `matrix`.
 
     Parameters
     ----------
     matrix : 2D array_like
-        Input array that requires its nearest symmetric positive-definite
-        variant.
+        Input matrix that requires its nearest positive-definite variant.
 
     Returns
     -------
-    mat_SPD : 2D array_like
-        The nearest symmetric positive-definite matrix to the input `matrix`.
+    mat_PD : 2D array_like
+        The nearest positive-definite matrix to the input `matrix`.
 
     Examples
     --------
-    Requesting the nearest SPD variant of a matrix that is already SPD results
+    Requesting the nearest PD variant of a matrix that is already PD results
     in it being returned immediately:
 
     >>> matrix = np.eye(3)
@@ -97,21 +117,44 @@ def nearest_SPD(matrix):
     array([[ 1.,  0.,  0.],
            [ 0.,  1.,  0.],
            [ 0.,  0.,  1.]])
-    >>> nearest_SPD(matrix)
+    >>> is_PD(matrix)
+    True
+    >>> nearest_PD(matrix)
     array([[ 1.,  0.,  0.],
            [ 0.,  1.,  0.],
            [ 0.,  0.,  1.]])
 
 
-    Using a non-SPD matrix results in it being transformed into an SPD-matrix:
+    Using a real non-PD matrix results in it being transformed into an
+    PD-matrix:
 
     >>> matrix = np.array([[1, 2], [3, 4]])
     >>> matrix
     array([[1, 2],
            [3, 4]])
-    >>> nearest_SPD(matrix)
+    >>> is_PD(matrix)
+    False
+    >>> mat_PD = nearest_PD(matrix)
+    >>> mat_PD
     array([[ 1.31461828,  2.32186616],
            [ 2.32186616,  4.10085767]])
+    >>> is_PD(mat_PD)
+    True
+
+
+    Using a complex non-PD matrix converts it into the nearest complex
+    PD-matrix:
+
+    >>> matrix = np.array([[4, 2+1j], [1+3j, 3]])
+    >>> matrix
+    array([[ 4.+0.j,  2.+1.j],
+           [ 1.+3.j,  3.+0.j]])
+    >>> mat_PD = nearest_PD(matrix)
+    >>> mat_PD
+    array([[ 4.0+0.j,  1.5-1.j],
+           [ 1.5+1.j,  3.0+0.j]])
+    >>> is_PD(matrix)
+    True
 
     Notes
     -----
@@ -119,13 +162,18 @@ def nearest_SPD(matrix):
     MATLAB implementation of Higham (1988) [2]_.
 
     According to Higham (1988), the nearest positive semi-definite matrix in
-    the Frobenius norm to an arbitrary real matrix A is shown to be
+    the Frobenius norm to an arbitrary real matrix :math:`A` is shown to be
 
         .. math:: \\frac{B+H}{2},
 
-    with H being the symmetric polar factor of
+    with :math:`H` being the symmetric polar factor of
 
         .. math:: B=\\frac{A+A^T}{2}.
+
+    On page 2, the author mentions that all matrices :math:`A` are assumed to
+    be real, but that the method can be very easily extended to the complex
+    case. This can indeed be done easily by taking the conjugate transpose
+    instead of the normal transpose in the formula on the above.
 
     References
     ----------
@@ -148,43 +196,44 @@ def nearest_SPD(matrix):
         raise ShapeError("Input matrix has shape [%s, %s]. Input matrix must "
                          "be a square matrix!" % (rows, columns))
 
-    # Check if matrix is real
-    if not(np.isreal(matrix).all()):
-        raise ValueError("Input matrix must be real!")
-
     # Check if matrix is not already positive-definite
-    if is_PD(matrix) is True:
-        return(matrix)
+    try:
+        is_PD(matrix)
+    except ValueError:
+        pass
+    else:
+        if is_PD(matrix) is True:
+            return(matrix)
 
-    # Make sure that the matrix is symmetrized
-    mat_sym = (matrix+transposeC(matrix))/2
+    # Make sure that the matrix is Hermitian
+    mat_H = (matrix+transposeC(matrix))/2
 
     # Perform singular value decomposition
-    U, S, VH = np.linalg.svd(mat_sym)
+    U, S, VH = np.linalg.svd(mat_H)
 
-    # Compute the symmetric polar factor of mat_sym
+    # Compute the symmetric polar factor of mat_H
     spf = np.dot(transposeC(VH), np.dot(np.diag(S), VH))
 
     # Obtain the positive-definite matrix candidate
-    mat_PD = (mat_sym+spf)/2
+    mat_PD = (mat_H+spf)/2
 
-    # Ensure that mat_PD is symmetric
-    mat_SPD = (mat_PD+transposeC(mat_PD))/2
+    # Ensure that mat_PD is Hermitian
+    mat_PD = (mat_PD+transposeC(mat_PD))/2
 
-    # Check if mat_SPD is in fact positive-definite
-    if is_PD(mat_SPD) is True:
-        return(mat_SPD)
+    # Check if mat_PD is in fact positive-definite
+    if is_PD(mat_PD) is True:
+        return(mat_PD)
 
     # If it is not, change it very slightly to make it positive-definite
     I = np.eye(rows)
     k = 1
     spacing = np.spacing(np.linalg.norm(matrix))
-    while is_PD(mat_SPD) is not True:
-        min_eig_val = np.min(np.real(np.linalg.eigvals(mat_SPD)))
-        mat_SPD += I*(-1*min_eig_val*pow(k, 2)+spacing)
+    while is_PD(mat_PD) is not True:
+        min_eig_val = np.min(np.real(np.linalg.eigvals(mat_PD)))
+        mat_PD += I*(-1*min_eig_val*pow(k, 2)+spacing)
         k += 1
     else:
-        return(mat_SPD)
+        return(mat_PD)
 
 
 def transposeC(array):
