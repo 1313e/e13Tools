db2851f783eaac9194f903fda9d530c27dd24ab3
1313e <ellert_vandervelden@outlook.com>
2017-11-06 20:51:23 +1100

    Finished doctests.

--- tests/test.py
+++ tests/test.py
@@ -1,23 +1,28 @@
 # -*- coding: utf-8 -*-
 """
-Created on Mon Nov  6 15:56:20 2017
+Testing program for e13Tools.
 
-@author: 1313e
 """
 
 import doctest
 from pkgutil import walk_packages
 import e13tools
 
+# Obtain list with all modules found in the package
 mod_list = [name for _, name, _ in walk_packages(e13tools.__path__,
                                                  e13tools.__name__+'.')]
+
 tests = doctest.DocTestSuite()
 
+# Make list of modules containing scripts that involve randomness
 skip_cases = ['e13tools.sampling.lhcs']
 
+# Make a unittest for every doctest found in the package
 for name in mod_list:
     if name in skip_cases:
         tests.addTests(doctest.DocTestSuite(name, optionflags=doctest.SKIP))
     else:
         tests.addTests(doctest.DocTestSuite(name))
+
+# Perform the unittest debug
 tests.debug()
